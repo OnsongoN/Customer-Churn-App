8036c4682b6189a9f79a4d806b0ba4d12eff85ab
OnsongoN <149000912+OnsongoN@users.noreply.github.com>
2024-02-18 22:09:06 +0300

    app pages addition

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.set_page_config(
-    page_title= 'Home',
-    page_icon= ':)',
-    layout= 'wide'
-)
 
 st.title('Home Page')
